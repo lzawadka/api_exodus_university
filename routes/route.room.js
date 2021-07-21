76dbbf67e99a9a7fcf16c1f971817ec26dcdafc3
Bottaro Gregorio <gbottaro@adilibre.fr>
2021-07-21 17:57:48 +0300

    room services

--- routes/route.room.js
+++ routes/route.room.js
@@ -5,7 +5,7 @@ const roomController = require("../controller/controller.room");
 
 router.get("/getRooms", auth, roomController.getAllRooms);
 router.get("/getRoomDetails", auth, roomController.getRoomDetails);
-router.get("/updateRoomLocked", auth, roomController.updateRoomLocked);
-router.get("/updateRoomCapacity", auth, roomController.updateRoomCapacity);
+router.post("/updateRoomLocked", auth, roomController.updateRoomLocked);
+router.post("/updateRoomCapacity", auth, roomController.updateRoomCapacity);
 
 module.exports = router;
